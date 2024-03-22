48b525f18dae0e18766b0a3db4cb6eeb56068b87
ogola <ogolajeremy601@gmail.com>
2024-03-22 08:59:07 +0300

    comp-array-lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendCat(){
+  cats.push('Ralph')
+  
+}
+function destructivelyPrependCat(){
+    cats.unshift('Bob')
+    
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+
+}
+function appendCat(){
+    let spread = [...cats, "Broom"]
+    return spread
+
+}
+function prependCat(){
+    let spread = ['Arnold',...cats]
+    return spread
+}
+function removeLastCat(){
+    let sliced = cats.slice(0,2)
+    return sliced
+}
+function removeFirstCat(){
+    let copyOfCats = cats.slice();
+    copyOfCats.shift(); 
+    return copyOfCats
+}
